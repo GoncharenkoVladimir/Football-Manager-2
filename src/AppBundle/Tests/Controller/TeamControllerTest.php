40c45f44a94721b56d76f1ebcd6b366c20a9c0f8
Salsa <vova.goncharenko@ekreative.com>
2015-12-04 16:15:51 +0200

    Tests

--- src/AppBundle/Tests/Controller/TeamControllerTest.php
+++ src/AppBundle/Tests/Controller/TeamControllerTest.php
@@ -3,7 +3,7 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class CommandControllerTest extends WebTestCase
+class TeamControllerTest extends WebTestCase
 {
     public function testIndex()
     {
