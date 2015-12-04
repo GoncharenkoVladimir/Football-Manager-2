a5dd61f955378c1af47161b90b65476b2fcd2d39
Salsa <vova.goncharenko@ekreative.com>
2015-12-04 12:52:08 +0200

    Tests

--- src/AppBundle/Tests/Controller/CountryControllerTest.php
+++ src/AppBundle/Tests/Controller/CountryControllerTest.php
@@ -5,23 +5,10 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CountryControllerTest extends WebTestCase
 {
-    /**
-     * @param $url
-     * @param $code
-     * @dataProvider testIndexProvider
-     */
-    public function testIndex($url, $code)
+    public function testIndex()
     {
         $client = static::createClient();
-        $client->request('GET', $url);
-        $this->assertEquals($code, $client->getResponse()->getStatusCode());
-    }
-
-    public function testIndexProvider()
-    {
-        return [
-            ['/country', 500],
-            ['/player', 404],
-        ];
+        $crawler = $client->request('GET', '/player/1');
+        $this->assertContains('Украина', $crawler->filter('.baner h3')->text());
     }
 }
