ca0ca1583878b7694b23b28226e2a2a8aa08bad8
Salsa <vova.goncharenko@ekreative.com>
2015-12-14 18:13:29 +0200

    Tests

--- src/AppBundle/Tests/Controller/PlayerControllerTest.php
+++ src/AppBundle/Tests/Controller/PlayerControllerTest.php
@@ -12,7 +12,6 @@ class PlayerControllerTest extends TestBaseWeb
         $client = static::createClient();
         $em = $client->getContainer()->get('doctrine.orm.entity_manager');
         $player = $em->getRepository('AppBundle:Player')->find(1);
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Player1', $player->getName());
     }
 }
