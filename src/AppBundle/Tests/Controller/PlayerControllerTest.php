ea21aaf07ac55b7baa65311147141ffba772935c
Salsa <vova.goncharenko@ekreative.com>
2015-12-14 17:51:44 +0200

    Tests

--- src/AppBundle/Tests/Controller/PlayerControllerTest.php
+++ src/AppBundle/Tests/Controller/PlayerControllerTest.php
@@ -9,8 +9,8 @@ class PlayerControllerTest extends TestBaseWeb
     public function testShow()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/players/1');
-        $this->assertContains('Player1:', $crawler->filter('.baner h3')->text());
-
+        $crawler = $client->request('GET', '/player/1');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Player1', $crawler->filter('.baner h3')->text());
     }
 }
