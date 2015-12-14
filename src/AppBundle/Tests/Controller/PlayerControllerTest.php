57c7f712c42ba252cb0189668d69bd0a48adc6af
Salsa <vova.goncharenko@ekreative.com>
2015-12-14 17:08:05 +0200

    Tests

--- src/AppBundle/Tests/Controller/PlayerControllerTest.php
+++ src/AppBundle/Tests/Controller/PlayerControllerTest.php
@@ -5,12 +5,18 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class PlayerControllerTest extends TestBaseWeb
 {
-    public function testIndex()
+    public function testShow()
     {
-        $this->tearDown();
-        $this->setUp();
         $client = static::createClient();
-        $crawler = $client->request('GET', '/player/1');
-        $this->assertContains('Player1', $crawler->filter('.baner h3')->text());
+        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
+        $slug = $em
+            ->getRepository('AppBundle:Country')
+            ->findOneBy([])->getSlug();
+        $crawler = $client->request('GET', "/country/{$slug}");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(
+            1,
+            $crawler->filter('h1')->count()
+        );
     }
 }
