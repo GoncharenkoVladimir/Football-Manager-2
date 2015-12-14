0337c7102d404f9fea108314fcecd7bdd3bc8b88
Salsa <vova.goncharenko@ekreative.com>
2015-12-14 17:16:01 +0200

    Tests

--- src/AppBundle/Tests/Controller/PlayerControllerTest.php
+++ src/AppBundle/Tests/Controller/PlayerControllerTest.php
@@ -7,16 +7,18 @@ class PlayerControllerTest extends TestBaseWeb
 {
     public function testShow()
     {
+        $this->tearDown();
+        $this->setUp();
         $client = static::createClient();
         $em = $client->getContainer()->get('doctrine.orm.entity_manager');
         $slug = $em
-            ->getRepository('AppBundle:Country')
+            ->getRepository('AppBundle:Player')
             ->find(1);
-        $crawler = $client->request('GET', "/country/{$slug}");
+        $crawler = $client->request('GET', "/player/{$slug}");
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertEquals(
             1,
-            $crawler->filter('h1')->count()
+            $crawler->filter('.baner h3')->count()
         );
     }
 }
