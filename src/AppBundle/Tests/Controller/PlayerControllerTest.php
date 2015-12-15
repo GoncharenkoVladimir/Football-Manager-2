39aa2ee437a51f508ada7eb5c3c33c3baa1e334a
Salsa <vova.goncharenko@ekreative.com>
2015-12-15 10:12:28 +0200

    Tests

--- src/AppBundle/Tests/Controller/PlayerControllerTest.php
+++ src/AppBundle/Tests/Controller/PlayerControllerTest.php
@@ -10,10 +10,11 @@ class PlayerControllerTest extends TestBaseWeb
     public function testShow()
     {
         $this->setUp();
-        $this->tearDown();
 
         $em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
         $player = $em->getRepository('AppBundle:Player')->find(1);
         $this->assertContains('Player1', $player->getName());
+
+        $this->tearDown();
     }
 }
